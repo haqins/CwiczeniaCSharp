de2d70745ffc62508e9603a8738b62431d25b5c9
haqins <haqins@icloud.com>
2022-03-06 14:23:07 +0100

    dodano klase CardPicker

--- PickRandomCards/CardPicker.cs
+++ PickRandomCards/CardPicker.cs
@@ -2,7 +2,14 @@ class CardPicker
 {
    public static string [] PickSomeCards(int numberOfCards)
    {
-       
+       string[] PickedCards = 
+       new string[numberOfCards];
+       for (int i =0; i < numberOfCards; i++;)
+       {
+           PickedCards[i] = RandomValue()+
+           "of" + RandomSuit();
+       }
+       return PickedCards;
    }
    public static RandomSuit ()
    {
